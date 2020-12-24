6c85f3970512ac1ab5cd7aadb0fa0f39179ab1d7
lusuijie <lusuijie@zhihu.com>
2020-12-24 17:49:06 +0800

    feat: 歌手列表页的开发

---
+++ cloud-music/src/application/Singers/store/index.js
@@ -0,0 +1,6 @@
+// 用来导出 reducer，action
+import reducer from './reducer'
+import * as actionCreators from './actionCreators'
+import * as constants from './constants'
+
+export { reducer, actionCreators, constants };
